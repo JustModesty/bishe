9ee2a7132b9d71df5669fc80efe9e6051191d3bf
JustModesty <381654728@qq.com>
2020-03-08 17:47:28 +0800

    添加了方便开发的调试方法，Web自动刷新

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 # bishe
-# 启动方式
+# 正常模式下的启动方式
 ```
 python app.py
+```
+
+## 调试模式的启动方式(Web页面自动刷新，方便开发)
+```
+python app.py dev
 ```
 
--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, session, redirect, url_for, request, render_template
-from markupsafe import escape
+from flask_script import Manager
 
 app = Flask(__name__)
-
+# 这一句方便开发
+app.config['DEBUG'] = True
+manager = Manager(app)
 
 # 主页，输入URL之后的页面，显示按钮《开始爬取》 《显示数据》 《清空数据》
 @app.route('/')
@@ -16,7 +18,7 @@ def start_spider():
     return render_template('start_spider.html')
 
 
-# “显示数据” 接口
+# “网站首页” 接口
 @app.route('/gdut_index')
 def gdut_index():
     return render_template('gdut_index.html')
@@ -28,9 +30,22 @@ def clear_data():
     return render_template('clear_data.html')
 
 
+# 方便开发，自动同步浏览器
+@manager.command
+def dev():
+    from livereload import Server
+    live_server = Server(app.wsgi_app)
+    live_server.watch('**/*.*')
+    live_server.serve(open_url_delay=True)
+    
+
 if __name__ == '__main__':
     # 正常模式
     # app.run()
 
     # 调试模式
-    app.run(debug=True)
+    # app.run(debug=True)
+
+    # 方便开发，自动同步更新浏览器
+    manager.run()
+    
