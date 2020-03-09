e8a50999276907c9c69d3f8e4c201c7230b5598b
JustModesty <381654728@qq.com>
2020-03-09 17:20:59 +0800

    初始化base

--- app.py
+++ app.py
@@ -1,21 +1,37 @@
+# -*- coding: utf-8 -*-
 from flask import Flask, session, redirect, url_for, request, render_template
 from flask_script import Manager
 
+
 from flask_bootstrap import Bootstrap
+from flask_nav import Nav
+from flask_nav.elements import *
+
 
 app = Flask(__name__)
+
+# 这一句方便开发
+app.config['DEBUG'] = True
 
 # 使用Bootstrap
 Bootstrap(app)
 
+# 使用菜单
+nav = Nav()
+
+
 # 这一句方便开发
-app.config['DEBUG'] = True
 manager = Manager(app)
+
+nav.register_element('top', Navbar(u'广东工业大学新闻网',
+                                   View(u'主页', 'index')))
+nav.init_app(app)
+
 
 # 主页，输入URL之后的页面，显示按钮《开始爬取》 《显示数据》 《清空数据》
 @app.route('/')
 def index():
-    return render_template('index.html', title="欢迎使用")
+    return render_template('index.html')
 
 
 # “开始爬取” 接口
@@ -43,7 +59,7 @@ def dev():
     live_server = Server(app.wsgi_app)
     live_server.watch('**/*.*')
     live_server.serve(open_url_delay=True)
-    
+
 
 if __name__ == '__main__':
     # 正常模式
@@ -54,4 +70,3 @@ if __name__ == '__main__':
 
     # 方便开发，自动同步更新浏览器
     manager.run()
-    
