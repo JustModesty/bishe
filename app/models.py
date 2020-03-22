e3abe6022fbbd73fd670108a55ccab2d10665038
JustModesty <381654728@qq.com>
2020-03-22 08:12:48 +0800

    saved

--- app/models.py
+++ app/models.py
@@ -1,8 +1,9 @@
 from . import db
 
-
-class HomePage(db.Model):
-    __tablename__ = 'home_page'
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String)
-    link = db.Column(db.String)
+#
+# class HomePage(db.Model):
+#     __tablename__ = 'home_page'
+#     id = db.Column(db.Integer, primary_key=True)
+#     title = db.Column(db.String)
+#     link = db.Column(db.String)
+#
--- manager.py
+++ manager.py
@@ -2,6 +2,7 @@
 from flask_script import Manager
 from app import create_app
 
+
 app = create_app()
 
 # 这一句方便开发
